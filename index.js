991081b46474197cade88af5fcd3316399542437
NicolasVarela23 <nicolas.varela@newsol.com.co>
2025-03-27 12:58:03 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const routerApi = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const cors = require('cors');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
